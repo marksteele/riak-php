dcf4f5942c79ff685f4cf8f4afce103c38ff72e5
Mark Steele <mark@control-alt-del.org>
2012-11-22 23:14:03 -0500

    bug fix

--- Riak/Transport/Pb.php
+++ Riak/Transport/Pb.php
@@ -553,7 +553,7 @@ class Riak_Transport_Pb extends Riak_Transport
     if ($presort) {
       $req->setPresort($presort);
     }
-    $this->_sendData($this->_encodeMessage($req, self:::MSG_CODE_SEARCH_QUERY_REQ));
+    $this->_sendData($this->_encodeMessage($req, self::MSG_CODE_SEARCH_QUERY_REQ));
     list($messageCode, $response) = $this->_receiveMessage();
     if ($messageCode == self::MSG_CODE__SEARCH_QUERY_RESP) {
       $results = array();
